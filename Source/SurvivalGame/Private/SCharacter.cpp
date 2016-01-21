05e3ed456f2cf1160eed93bbc38f30d72f98852f
enki390 <enki390@gmail.com>
2016-01-22 00:41:42 +0900

    * Studying Section - 01

--- Source/SurvivalGame/Private/SCharacter.cpp
+++ Source/SurvivalGame/Private/SCharacter.cpp
@@ -44,5 +44,156 @@ void ASCharacter::SetupPlayerInputComponent(class UInputComponent* InputComponen
 {
 	Super::SetupPlayerInputComponent(InputComponent);
 
+	InputComponent->BindAxis("MoveForward", this, &ASCharacter::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &ASCharacter::MoveRight);
+	InputComponent->BindAxis("Turn", this, &ASCharacter::AddControllerYawInput);
+	InputComponent->BindAxis("LookUp", this, &ASCharacter::AddControllerPitchInput);
+
+	InputComponent->BindAction("SprintHold", IE_Pressed, this, &ASCharacter::OnStartSprinting);
+	InputComponent->BindAction("SprintHold", IE_Released, this, &ASCharacter::OnStopSprinting);
+	InputComponent->BindAction("CrouchToggle", IE_Released, this, &ASCharacter::OnCrounchToggle);
+	InputComponent->BindAction("Jump", IE_Pressed, this, &ASCharacter::OnStartJump);
+	InputComponent->BindAction("Jump", IE_Released, this, &ASCharacter::OnStopJump);
 }
 
+void ASCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	//DOREPLIFETIME_CONDITION(ASCharacter, bWantsToRun, COND_SkipOwner);
+	//DOREPLIFETIME_CONDITION(ASCharacter, bIsJumping, COND_SkipOwner);
+}
+
+void ASCharacter::MoveForward(float inVal)
+{
+	UCharacterMovementComponent* lMovementComp = GetCharacterMovement();
+	if (Controller && lMovementComp && inVal != 0.f)
+	{
+		const bool lbLimitRotation = (lMovementComp->IsMovingOnGround() || lMovementComp->IsFalling());
+		const FRotator lRotation = lbLimitRotation ? GetActorRotation() : Controller->GetControlRotation();
+		const FVector lDirection = FRotationMatrix(lRotation).GetScaledAxis(EAxis::X);
+		AddMovementInput(lDirection, inVal);
+	}
+}
+
+void ASCharacter::MoveRight(float inVal)
+{
+	if (inVal != 0.f)
+	{
+		const FRotator lRotation = GetActorRotation();
+		const FVector lDirection = FRotationMatrix(lRotation).GetScaledAxis(EAxis::Y);
+		AddMovementInput(lDirection, inVal);
+	}
+}
+
+void ASCharacter::OnCrounchToggle()
+{
+	if (CanCrouch())
+	{
+		Crouch();
+	}
+	else
+	{
+		UnCrouch();
+	}
+}
+
+void ASCharacter::OnStartJump()
+{
+	bPressedJump = true;
+	SetIsJumping(true);
+}
+
+void ASCharacter::OnStopJump()
+{
+	bPressedJump = false;
+}
+
+void ASCharacter::OnStartSprinting()
+{
+	SetSprinting(true);
+}
+
+void ASCharacter::OnStopSprinting()
+{
+	SetSprinting(false);
+}
+
+bool ASCharacter::IsInitiatedJump() const
+{
+	return bIsJumping;
+}
+
+void ASCharacter::SetIsJumping(bool inIsJumping)
+{
+	if (bIsCrouched && inIsJumping)
+	{
+		UnCrouch();
+	}
+	else
+	{
+		bIsJumping = inIsJumping;
+	}
+}
+
+//void ASCharacter::ServerSetIsJumping(bool inIsJumping)
+//{
+//}
+//
+//void ASCharacter::ServerSetIsJumping_Implementation(bool inIsJumping)
+//{
+//	SetIsJumping(inIsJumping);
+//}
+//
+//bool ASCharacter::ServerSetIsJumping_Validate(bool inIsJumping)
+//{
+//	return true;
+//}
+
+void ASCharacter::Landed(const FHitResult & inHit)
+{
+	Super::Landed(inHit);
+	SetIsJumping(false);
+}
+
+void ASCharacter::SetSprinting(bool inIsSprinting)
+{
+	bWantsToRun = inIsSprinting;
+	if (bIsCrouched)
+	{
+		UnCrouch();
+	}
+
+	// TODO: Stop weapon fire
+
+	//if (Role < ROLE_Authority)
+	//{
+	//	ServerSetSprinting(inIsSprinting);
+	//}
+}
+
+//void ASCharacter::ServerSetSprinting(bool inIsSprinting)
+//{}
+//
+//void ASCharacter::ServerSetSprinting_Implementation(bool inIsSprinting)
+//{
+//	SetSprinting(inIsSprinting);
+//}
+//
+//bool ASCharacter::ServerSetSprinting_Validate(bool inIsSprinting)
+//{
+//	return true;
+//}
+
+bool ASCharacter::IsSprinting() const
+{
+	if (!GetCharacterMovement())
+		return false;
+	return bWantsToRun && !GetVelocity().IsZero() &&
+		   (GetVelocity().GetSafeNormal2D() | GetActorRotation().Vector()) > 0.8;
+}
+
+float ASCharacter::GetSprintingSpeedModifier() const
+{
+	return SprintingSpeedModifier;
+}
--- Source/SurvivalGame/Private/SCharacter.h
+++ Source/SurvivalGame/Private/SCharacter.h
@@ -28,6 +28,53 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
 
+	/***************************************************************************************/
+	/* Movement
+	/***************************************************************************************/
+	virtual void MoveForward(float inVal);
+	virtual void MoveRight(float inVal);
+
+	void OnCrounchToggle();
+	void OnStartJump();
+	void OnStopJump();
+	void OnStartSprinting();
+	void OnStopSprinting();
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	bool IsInitiatedJump() const;
+
+	/** Client call to update jumping state */
+	void SetIsJumping(bool inIsJumping);
 	
+	///** Server side call to update actual jumping state */
+	//UFUNCTION(Server, Server, WithValidation)
+	//void ServerSetIsJumping(bool inIsJumping);
 	
+	/** It's just reset jumping state */
+	virtual void Landed(const FHitResult& inHit) override;
+
+	/** Client call to update sprint state */
+	void SetSprinting(bool inIsSprinting);
+
+	///** Server side calll to update actual sprint state */
+	//UFUNCTION(Server, Reliable, WithValidation)
+	//void ServerSetSprinting(bool inIsSprinting);
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+	bool IsSprinting() const;
+
+	float GetSprintingSpeedModifier() const;
+
+	/** Is character wants to run */
+	UPROPERTY(Transient, Replicated)
+	bool bWantsToRun;
+
+	/** Is character currently performing a jump action? */
+	UPROPERTY(Transient, Replicated)
+	bool bIsJumping;
+
+	/** Speed modidy factor when sprinting */
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float SprintingSpeedModifier;
+
 };
